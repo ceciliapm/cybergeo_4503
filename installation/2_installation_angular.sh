b8559d5cb25fb07cbee28c4f4e12d3f309af8cbb
Julien Hubert <jhubert@forum-marais-atl.com>
2025-04-25 10:54:00 +0200

    Upload New File

---
+++ installation/2_installation_angular.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Installation npm
+apt-get install npm --force-yes
+
+# Installation Angular
+npm install -g @angular/cli@13.1 --force-yes
+
+# Installation NodeJS
+apt-get install nodejs --force-yes
+
