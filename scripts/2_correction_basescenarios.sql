c7ba8a94dcab9ac4bbb27007484f76ad1b0cae29
Julien Hubert <jhubert@forum-marais-atl.com>
2025-04-25 12:19:41 +0200

    Upload New File

---
+++ scripts/2_correction_basescenarios.sql
@@ -0,0 +1,197 @@
+update scenarios.base_scenarios 
+set objet = 'Saliculture'
+where nom_vue = 'vue_csc_sel_fa';
+update scenarios.base_scenarios 
+set objet = 'Coque glauque'
+where nom_vue = 'vue_cn_cerastoderma_fa_cor';
+update scenarios.base_scenarios 
+set objet = 'Végétation dominante de marais salés'
+where nom_vue = 'vue_cn_vegetation_dom_mo';
+update scenarios.base_scenarios 
+set objet = 'Ressource en eau douces'
+where nom_vue = 'vue_cp_ress_eau_douce_mo';
+update scenarios.base_scenarios 
+set objet = 'Pré-salés'
+where nom_vue = 'vue_cp_presale_fa';
+update scenarios.base_scenarios 
+set objet = 'Pêche de loisir'
+where nom_vue = 'vue_csc_peche_loisir_ta';
+update scenarios.base_scenarios 
+set objet = 'Salicorne'
+where nom_vue = 'vue_cn_salicorne_fa';
+update scenarios.base_scenarios 
+set objet = 'Prairies naturelles et Troupeaux'
+where nom_vue = 'vue_cp_prairie_naturelle_mo';
+update scenarios.base_scenarios 
+set objet = 'Réserve naturelle nationale de Lilleau des Niges'
+where nom_vue = 'vue_cp_reserve_lilleau_fa';
+update scenarios.base_scenarios 
+set objet = 'Carbone'
+where nom_vue = 'vue_cn_carbone_fa';
+update scenarios.base_scenarios 
+set objet = 'Moutarde sauvage'
+where nom_vue = 'vue_cn_moutarde_fa';
+update scenarios.base_scenarios 
+set objet = 'Crapaud epineux'
+where nom_vue = 'vue_cn_crapaud_epi_ta';
+update scenarios.base_scenarios 
+set objet = 'Roselière'
+where nom_vue = 'vue_cn_roseliere_mo';
+update scenarios.base_scenarios 
+set nom_vue = 'vue_cna_flore_roseliere_mo'
+where nom_vue = 'vue_cn_roseliere_mo';
+update scenarios.base_scenarios 
+set objet = 'Outils traditionnels de la saliculture'
+where nom_vue = 'vue_sel_fa';
+update scenarios.base_scenarios 
+set objet = 'Obione'
+where nom_vue = 'vue_cn_obione_fa';
+update scenarios.base_scenarios 
+set objet = 'Structure traditionnelles des marais salants'
+where nom_vue = 'vue_cp_str_trad_fa';
+update scenarios.base_scenarios 
+set objet = 'Ouvrages hydrauliques et écluses'
+where nom_vue = 'vue_cp_ouvrages_hydrau_ta';
+update scenarios.base_scenarios 
+set objet = 'Ouvrages hydrauliques et écluses'
+where nom_vue = 'vue_cp_ouvrages_hydrau_fa';
+update scenarios.base_scenarios 
+set objet = 'Roseau'
+where nom_vue = 'vue_cn_roseau_ta';
+update scenarios.base_scenarios 
+set objet = 'Carbone'
+where nom_vue = 'vue_cn_carbone_mo';
+update scenarios.base_scenarios 
+set objet = 'Carbone'
+where nom_vue = 'vue_cn_carbone_ta';
+update scenarios.base_scenarios 
+set objet = 'Lavande de mer (statice)'
+where nom_vue = 'vue_cn_lavande_fa';
+update scenarios.base_scenarios 
+set objet = 'Végétation dominante des marais salés'
+where nom_vue = 'vue_cn_vegetation_dom_fa';
+update scenarios.base_scenarios 
+set objet = 'Pré-salés'
+where nom_vue = 'vue_cp_presale_mo';
+update scenarios.base_scenarios 
+set objet = 'Tonnes de chasse'
+where nom_vue = 'vue_cp_tonnes_chasse_mo';
+update scenarios.base_scenarios 
+set objet = 'Vasais'
+where nom_vue = 'vue_cp_vasais_fa';
+update scenarios.base_scenarios 
+set objet = 'Huîtres ostréicoles'
+where nom_vue = 'vue_csc_huitres_ostr_fa';
+update scenarios.base_scenarios 
+set objet = 'Huîtres ostréicoles'
+where nom_vue = 'vue_csc_huitres_ostr_mo';
+update scenarios.base_scenarios 
+set objet = 'Ostréiculture'
+where nom_vue = 'vue_csc_ostreiculture_fa';
+update scenarios.base_scenarios 
+set objet = 'Pastoralisme'
+where nom_vue = 'vue_csc_pastoralisme_mo';
+
+update scenarios.base_scenarios 
+set id_fiche = 3193
+where nom_vue = 'vue_cp_presale_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3489
+where nom_vue = 'vue_cp_tonnes_chasse_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3266
+where nom_vue = 'vue_cp_vasais_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3263
+where nom_vue = 'vue_csc_huitres_ostr_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3524
+where nom_vue = 'vue_csc_huitres_ostr_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3278
+where nom_vue = 'vue_csc_ostreiculture_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3493
+where nom_vue = 'vue_csc_pastoralisme_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3526
+where nom_vue = 'vue_cna_flore_roseliere_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3254
+where nom_vue = 'vue_csc_sel_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3280
+where nom_vue = 'vue_cn_cerastoderma_fa_cor';
+update scenarios.base_scenarios 
+set id_fiche = 3323
+where nom_vue = 'vue_cn_vegetation_dom_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3208
+where nom_vue = 'vue_cp_ress_eau_douce_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3192
+where nom_vue = 'vue_cp_presale_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3225
+where nom_vue = 'vue_csc_peche_loisir_ta';
+update scenarios.base_scenarios 
+set id_fiche = 3230
+where nom_vue = 'vue_cn_salicorne_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3530
+where nom_vue = 'vue_cp_prairie_naturelle_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3271
+where nom_vue = 'vue_cp_reserve_lilleau_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3494
+where nom_vue = 'vue_cn_carbone_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3327
+where nom_vue = 'vue_cn_moutarde_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3627
+where nom_vue = 'vue_cn_crapaud_epi_ta';
+update scenarios.base_scenarios 
+set id_fiche = 3526
+where nom_vue = 'vue_cn_roseliere_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3272
+where nom_vue = 'vue_sel_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3326
+where nom_vue = 'vue_cn_obione_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3191
+where nom_vue = 'vue_cp_str_trad_fa';
+update scenarios.base_scenarios 
+set id_fiche = 3186
+where nom_vue = 'vue_cp_ouvrages_hydrau_ta';
+update scenarios.base_scenarios 
+set id_fiche = 3516
+where nom_vue = 'vue_cn_roseau_ta';
+update scenarios.base_scenarios 
+set id_fiche = 3513
+where nom_vue = 'vue_cn_carbone_mo';
+update scenarios.base_scenarios 
+set id_fiche = 3512
+where nom_vue = 'vue_cn_carbone_ta';
+update scenarios.base_scenarios 
+set id_fiche = 3328
+where nom_vue = 'vue_cn_lavande_fa';
+
+
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Fleur de Sel', nom_vue, indice, 3258::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_1;
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Fleur de Sel', nom_vue, indice, 3258::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_2;
+
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Gros Sel', nom_vue, indice, 3259::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_1;
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Gros Sel', nom_vue, indice, 3259::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_2;
+
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Sel', nom_vue, indice, 3188::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_1;
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+select id_scenario, 'Sel', nom_vue, indice, 3188::integer as id_fiche, comp, fct, id_site from resultats_sensibilite.vue_csc_sel_fa_1_2;
