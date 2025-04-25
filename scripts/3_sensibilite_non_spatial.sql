5f2a46f420804757bc6854fe8d7e185f78241d3c
Julien Hubert <jhubert@forum-marais-atl.com>
2025-04-25 12:19:56 +0200

    Upload New File

---
+++ scripts/3_sensibilite_non_spatial.sql
@@ -0,0 +1,53 @@
+-- Savoir faire ostréicole FA
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('1_1', 'Savoir-faire ostréicole', 'vue_dis_savoirfaire_ostreicole', 1, 3274, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '1');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('1_2', 'Savoir-faire ostréicole', 'vue_dis_savoirfaire_ostreicole', 1, 3274, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '1');
+
+-- Savoir faire des sauniers
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('1_1', 'Savoir-faire des sauniers', 'vue_dis_savoirfaire_sauniers', 1, 3251, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '1');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('1_2', 'Savoir-faire des sauniers', 'vue_dis_savoirfaire_sauniers', 1, 3251, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '1');
+
+-- Agrion de Mercure
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('2_1', 'Agrion de Mercure (libellule)', 'vue_dis_agrion_mercure', 1, 3329, '', 'Fonction patrimoniale environnementale', '2');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('2_2', 'Agrion de Mercure (libellule)', 'vue_dis_agrion_mercure', 1, 3329, '', 'Fonction patrimoniale environnementale', '2');
+
+-- Histoire de l'ancien golfe de saintonge
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Histoire de l''ancien Golfe de Saintonge', 'vue_dis_histoire_golfe_saintonge', 1, 3204, '', 'Fonction patrimoniale culturelle', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Histoire de l''ancien Golfe de Saintonge', 'vue_dis_histoire_golfe_saintonge', 1, 3204, '', 'Fonction patrimoniale culturelle', '3');
+
+-- Patois saintongeais
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Patois saintongeais', 'vue_dis_patois_saintongeais', 1, 3536, '', 'Fonction patrimoniale culturelle', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Patois saintongeais', 'vue_dis_patois_saintongeais', 1, 3536, '', 'Fonction patrimoniale culturelle', '3');
+
+-- Savoir-faire de la chasse
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Savoir-faire de la chasse', 'vue_dis_savoirfaire_chasse', 1, 3525, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale paysagère', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Savoir-faire de la chasse', 'vue_dis_savoirfaire_chasse', 1, 3525, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale paysagère', '3');
+
+-- Savoir faire ostréicole BR
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Savoir-faire ostréicole', 'vue_dis_savoirfaire_ostreicole', 1, 3528, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Savoir-faire ostréicole', 'vue_dis_savoirfaire_ostreicole', 1, 3528, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '3');
+
+-- Savoir-faire de la pêche fluviale
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Savoir-faire de la pêche fluviale', 'vue_dis_savoirfaire_pechefluviale', 1, 3488, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Savoir-faire de la pêche fluviale', 'vue_dis_savoirfaire_pechefluviale', 1, 3488, '', 'Fonction patrimoniale culturelle;Fonction patrimoniale productive;Fonction patrimoniale environnementale', '3');
+
+-- Marais en tant que propriété foncière
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_1', 'Marais en tant que propriété foncière', 'vue_dis_marais_propriete_fonciere', 1, 3207, '', 'Fonction patrimoniale culturelle', '3');
+insert into scenarios.base_scenarios (id_scenario, objet, nom_vue, indice, id_fiche, composante, fct, id_site) 
+VALUES ('3_2', 'Marais en tant que propriété foncière', 'vue_dis_marais_propriete_fonciere', 1, 3207, '', 'Fonction patrimoniale culturelle', '3');
